9438d4c904facb7634915ebea677a28b7424057d
ll6348 <104443468+ll6348@users.noreply.github.com>
2025-03-28 11:45:40 +0530

    Update snake_and_ladder.py

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -10,7 +10,6 @@ class SnakeLadderGame:
         print("Game initialized. Player starts at position 0.")
 
     def roll_dice(self):  
-        input("Press Enter to roll the dice.")  # User must press Enter before rolling
         dice_value = random.randint(1, 6)  # Generate a number between 1 and 6
         print(f"Player rolled a {dice_value}")
         return dice_value
